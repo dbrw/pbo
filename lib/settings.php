6ad41f87973c0acd8559caa652626c2de12aa223
dbrw <mailme@dhimas.org>
2018-05-22 17:27:11 +0700

    Menambahkan setting BASE_PATH
    Setting BASE_PATH untuk memberitahu aplikasi bahwa dia berada di dalam folder.
    Menambahkan komentar di `lib/setting.php` juga.

--- lib/settings.php
+++ lib/settings.php
@@ -2,10 +2,57 @@
 
 if(!defined('VERSION')) die();
 
+/*
+ * @const CONTROLLER_DIR folder tempat file-file controller disimpan
+ *
+ */
 define('CONTROLLER_DIR', 'Controller/');
+
+/*
+ * @const VIEWS_DIR folder tempat file-file view disimpan
+ *
+ */
 define('VIEWS_DIR', 'Views/');
+
+/*
+ * @const DB_HOST host untuk database, biasanya bernilai `localhost`
+ *
+ */
 define('DB_HOST', 'mariadb');
+
+/*
+ * @const DB_USER pengguna yang diperbolehkan mengakses database
+ *
+ */
 define('DB_USER', 'root');
+
+/*
+ * @const DB_PASS password untuk pengguna DB_USER
+ *
+ */
 define('DB_PASS', 'root');
+
+/*
+ * @const DB_NAME nama database
+ *
+ */
 define('DB_NAME', 'tugasku');
+
+/*
+ * @const DEBUG ubah menjadi `false` jika tidak dalam development
+ *
+ */
 define('DEBUG', true);
+
+/*
+ * @const BASE_PATH isikan dengan nama folder tempat file `index.php` berada dengan diawali garing (`/`).
+ * Contoh: `/pbo`
+ *
+ */
+define('BASE_PATH', '/pbo');
+
+/*
+ * @const BASE_URL isikan dengan FQDN. Contoh: `http://pbo.test`
+ *
+ */
+define('BASE_URL', '');
